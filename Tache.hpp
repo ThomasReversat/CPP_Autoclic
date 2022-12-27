83cb8d858b86f1b1dff77aef09f830cfaa95d0fa
bilel233 <47856884+bilel233@users.noreply.github.com>
2022-12-27 12:01:56 +0100

    Update Tache.hpp

--- Tache.hpp
+++ Tache.hpp
@@ -1,11 +1,11 @@
 
 #ifndef AUTOCLICKER_PROJET_TACHE_HPP
 #define AUTOCLICKER_PROJET_TACHE_HPP
-
 #include <iostream>
 #include <string>
 #include <windows.h>
 #include <vector>
+#include <fstream>
 using namespace std;
 
 class Tache
@@ -19,6 +19,8 @@ public:
     string ajouterTache(string t);
     string renommerTache(string mot);
     void historiqueTaches(vector <string>tab,string tache,int i); // insere une tache à la ieme position
+    void renommer_tache(string const nomFichier);
+
 };
 
 
