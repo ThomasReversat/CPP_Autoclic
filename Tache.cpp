0af0f8c96cc3150d1979ddc16970dd24800b2f69
bilel233 <47856884+bilel233@users.noreply.github.com>
2022-12-29 14:49:51 +0100

    Update Tache.cpp

--- Tache.cpp
+++ Tache.cpp
@@ -61,3 +61,30 @@ void Tache::renommer_tache(const string nomFichier)
         cout << "ERREUR: Impossible d'ouvrir le fichier." << endl;
     }
   //  monFlux.close();
+}
+
+void Tache::renommer_tache_lire(const string nomFichier)
+{
+    /* recupere le texte ecrit par l'utilisateur dans un fichier */
+
+    ifstream fichier("C:\\Users\\bilel\\Desktop\\CPP/renommer.txt");
+
+    if(fichier)
+    {
+        //L'ouverture s'est bien passée, on peut donc lire
+
+        string ligne; //Une variable pour stocker les lignes lues
+
+        while(getline(fichier, ligne)) //Tant qu'on n'est pas à la fin, on lit
+        {
+            cout << ligne << endl;
+            //Et on l'affiche dans la console
+            //Ou alors on fait quelque chose avec cette ligne
+            //À vous de voir
+        }
+    }
+    else
+    {
+        cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
+    }
+}
